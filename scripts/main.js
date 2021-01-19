3829a69ae5ac1d20c8b74fb424cd0b73c1ce2880
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-01-19 09:03:00 +0000

    Added mobile collapsible nav menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,7 @@
 $(function() {
     $("#navbar-container").load("/html/navbar.html");
-});
 
-function setActivePage() {
-    var page_title_id;
-    page_title_id = $(document).find("title").text()
-    if (page_title_id == "Home") {
-        $("#home-button").addClass("active");
-        //$("p").css("color", "red")
-        console.log("Home test");
-    } else if (page_title_id == "Resume") {
-        $("#resume-button").addClass("active");
-        //$("p").css("color", "yellow")
-        console.log("Resume test");
-    }
-}
+    $("#navbar-button").click(function(){
+        $("#navbar-container").slideToggle("125");
+      });
+});
