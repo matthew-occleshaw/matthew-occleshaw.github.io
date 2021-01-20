e67b2d9e333864a62fce5216ff7d5de1c6c757e3
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-01-20 20:14:57 +0000

    Added animation toggling

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,8 @@
 $(function() {
     $("#navbar-container").load("/html/navbar.html");
 
-    $("#navbar-menu-button").click(function(){
-        $("#navbar-container").slideToggle("125");
-        /*let i = 0;
-        if (i == 0) {
-          i++;
-        }*/
-        $("#navbar-menu-button:before").css({"transform": "translateY(12px) rotate(135deg)"})
-        $("#navbar-menu-button:after").css({"transform": "translateY(-12px) rotate(-135deg)"})
-        $("#navbar-menu-button div").css({"transform": "scale(0)"})
+    $(".navbar-menu-button").click(function(){
+        $("#navbar-container").slideToggle("125")
+        $(this).toggleClass("active")
       });
 });
-
-/*        #navbar-menu-button:before {
-            transform: translateY(12px) rotate(135deg);
-        }
-        #navbar-menu-button:after {
-            transform: translateY(-12px) rotate(-135deg);
-        }
-        #navbar-menu-button div {
-            transform: scale(0);
-        }
-*/
