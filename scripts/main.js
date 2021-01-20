afcd1e722843dddde73a0605c6af97aa093b3a46
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-01-20 19:42:21 +0000

    Pure CSS Hamburger

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,23 @@ $(function() {
 
     $("#navbar-menu-button").click(function(){
         $("#navbar-container").slideToggle("125");
+        /*let i = 0;
+        if (i == 0) {
+          i++;
+        }*/
+        $("#navbar-menu-button:before").css({"transform": "translateY(12px) rotate(135deg)"})
+        $("#navbar-menu-button:after").css({"transform": "translateY(-12px) rotate(-135deg)"})
+        $("#navbar-menu-button div").css({"transform": "scale(0)"})
       });
 });
+
+/*        #navbar-menu-button:before {
+            transform: translateY(12px) rotate(135deg);
+        }
+        #navbar-menu-button:after {
+            transform: translateY(-12px) rotate(-135deg);
+        }
+        #navbar-menu-button div {
+            transform: scale(0);
+        }
+*/
