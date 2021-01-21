7c9838b406fa96b9a4363bf430c4a8c47158a144
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-01-21 20:47:06 +0000

    Added footer

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 $(function() {
     $("#navbar-container").load("/html/navbar.html");
+    $("#footer").load("/html/footer.html");
 
     $(".navbar-menu-button").click(function(){
         $("#navbar-container").slideToggle("125")
