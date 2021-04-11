56b71798404064435cd0c4d66ccca0ea95fdc350
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-04-11 11:32:48 +0100

    Header now generated and added with js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 $(function() {
-    $("#navbar-container").load("/html/navbar.html", () => {
-      $(`#${document.title.toLowerCase()}-navbar-button`).addClass("active");
-    });
+  $.get("/html/header.html", (data) => {
+    $("header").append(data.replace("{{TITLE}}", document.title))
+    $(`#${document.title.toLowerCase()}-navbar-button`).addClass("active");
+  });
 
-    $("#footer").load("/html/footer.html");
+  $("#footer").load("/html/footer.html");
 
-    $(".navbar-hamburger").click(function() {
-        $("#navbar-container").slideToggle(250);
-        $(this).toggleClass("active");
-      });
+  $(".navbar-hamburger").click(function() {
+    $("#navbar-container").slideToggle(250);
+    $(this).toggleClass("active");
+  });
 });
