6c8b47bf6a5f96e786de54619d5585dfaea3f7d9
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-04-11 10:21:45 +0100

    Added current page navbar button highlighting

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
-$(function() {
-    $("#navbar-container").load("/html/navbar.html");
+$(() => {
+    $("#navbar-container").load("/html/navbar.html", () => {
+      $(`#${document.title.toLowerCase()}-navbar-button`).addClass("active");
+    });
+
     $("#footer").load("/html/footer.html");
 
     $(".navbar-menu-button").click(function(){
-        $("#navbar-container").slideToggle(250)
-        $(this).toggleClass("active")
+        $("#navbar-container").slideToggle(250);
+        $(this).toggleClass("active");
       });
 });
