465d5251c73f3e4e773385a114be419a27e4ce96
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-04-11 11:03:20 +0100

    Renamed navbar-menu-button to navbar-hamburger

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-$(() => {
+$(function() {
     $("#navbar-container").load("/html/navbar.html", () => {
       $(`#${document.title.toLowerCase()}-navbar-button`).addClass("active");
     });
 
     $("#footer").load("/html/footer.html");
 
-    $(".navbar-menu-button").click(function(){
+    $(".navbar-hamburger").click(function() {
         $("#navbar-container").slideToggle(250);
         $(this).toggleClass("active");
       });
