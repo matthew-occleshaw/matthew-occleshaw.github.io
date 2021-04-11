dbb1494f9d43377b122450eb368777681095c3e3
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-04-11 12:00:16 +0100

    Fixed hamburger menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
 $(function() {
+
   $.get("/html/header.html", (data) => {
-    $("header").append(data.replace("{{TITLE}}", document.title))
+
+    $("header").html(data.replace("{{TITLE}}", document.title));
     $(`#${document.title.toLowerCase()}-navbar-button`).addClass("active");
+
+    $(".navbar-hamburger").click(function() {
+      $("#navbar-container").slideToggle(250);
+      $(this).toggleClass("active");
+    });
+    
   });
 
   $("#footer").load("/html/footer.html");
 
-  $(".navbar-hamburger").click(function() {
-    $("#navbar-container").slideToggle(250);
-    $(this).toggleClass("active");
-  });
 });
