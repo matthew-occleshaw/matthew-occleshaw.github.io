593710946a2ad4c971fe159d136c7909e2ae2484
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-04-16 21:16:49 +0100

    Switched to using static site builder
    Now need to use https://github.com/matthew-occleshaw/personal-site-builder to build website

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,6 @@
 $(function() {
-
-  $.get("/html/header.html", (data) => {
-
-    $("header").html(data.replace("{{TITLE}}", document.title));
-    $(`#${document.title.toLowerCase()}-navbar-button`).addClass("active");
-
-    $(".navbar-hamburger").click(function() {
-      $("#navbar-container").slideToggle(250);
-      $(this).toggleClass("active");
-    });
-    
+  $(".navbar-hamburger").click(function() {
+    $("#navbar-container").slideToggle(250);
+    $(this).toggleClass("active");
   });
-
-  $("#footer").load("/html/footer.html");
-
 });
