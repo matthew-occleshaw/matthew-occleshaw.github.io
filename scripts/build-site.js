b9002de1400d633d108221a0cabaf94176374dc0
matthew-occleshaw <61629073+matthew-occleshaw@users.noreply.github.com>
2021-04-17 09:36:16 +0100

    Create build-site.js

---
+++ scripts/build-site.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path')
+const cheerio = require('cheerio');
+
+const websitePath = path.join(__dirname, '..')
+const sourcePath = path.join(websitePath, 'html', 'src', 'content');
+const destPath = path.join(websitePath, 'html', 'dest');
+
+let header = fs.readFileSync(path.join(websitePath, '/html/src/components/header.html'));
+let footer = fs.readFileSync(path.join(websitePath, './html/src/components/footer.html'));
+
+const files = fs.readdirSync(sourcePath);
+
+files.forEach(function (fileName) {
+  let oldFilePath = path.join(sourcePath, fileName);
+  if (fileName == 'index.html') {
+    var newFilePath = path.join(websitePath, 'index.html');
+  } else {
+    var newFilePath = path.join(destPath, fileName);
+  }
+  buildFile(oldFilePath, newFilePath);
+});
+
+function buildFile(oldFilePath, newFilePath) {
+
+  fs.copyFileSync(oldFilePath, newFilePath);
+  let file = fs.readFileSync(newFilePath);
+  let $ = cheerio.load(file);
+  
+  let title = $('title').text();
+
+  $('header').html(header);
+  $('footer').html(footer);
+  $('#page-title').html(title);
+
+  $(`#${title.toLowerCase()}-navbar-button`).addClass('active');
+
+  $.html();
+
+  fs.writeFileSync(newFilePath, $.html());
+
+}
