1bae5bbc8ae0d2697aa97fa8b9c05850d1fa4b99
Matthew Occleshaw <>
2021-05-14 12:10:59 +0000

    Add gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('build', (done) => {
-  console.log('poggers');
-  done();
-});
+function sass (cb) {
+  return gulp.src('styles/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(gulpSass())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('styles/css/'));
+  cb()
+};
+
+exports.sass = sass;
